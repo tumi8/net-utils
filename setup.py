ccaae46c4895446a7dbf3d7867d7572fc1b14df3
Patrick Sattler <sattler.patrick@posteo.de>
2017-05-17 17:43:55 +0200

    add setup py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+install_requires = ['IP2Location>=8.0.0', 'numpy>=1.12',]
+
+setup(name='I8 Net-Utils',
+      version='0.1',
+      description='Some network utilities from the TUM I8 chair of network architectures and '
+                  'services',
+      author='TUM I8',
+      author_email='some-email-address@net.in.tum.de',
+      url='https://github.com/tumi8/net-utils',
+      packages=['netutils'],
+      install_requires=install_requires,
+     )
