2f2489509db4077e4b84ef2aadf157ba77070526
Patrick Sattler <sattler@users.noreply.github.com>
2017-05-22 17:50:49 +0200

    add Quirins email as author email

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ install_requires = ['IP2Location>=8.0.0', 'numpy>=1.12',]
 
 setup(name='I8 Net-Utils',
       version='0.1',
-      description='Some network utilities from the TUM I8 chair of network architectures and '
+      description='Different network utilities from the TUM I8 chair of network architectures and '
                   'services',
       author='TUM I8',
-      author_email='some-email-address@net.in.tum.de',
+      author_email='scheitle@net.in.tum.de',
       url='https://github.com/tumi8/net-utils',
       packages=['netutils'],
       install_requires=install_requires,
